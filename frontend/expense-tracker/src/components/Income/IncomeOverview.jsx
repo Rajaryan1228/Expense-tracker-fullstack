fbbc802bb298f4f3f9deafdd2049d02cc035c341
Rajaryan1228 <160250166+Rajaryan1228@users.noreply.github.com>
2025-09-21 02:09:02 +0530

    Update IncomeOverview.jsx

--- frontend/expense-tracker/src/components/Income/IncomeOverview.jsx
+++ frontend/expense-tracker/src/components/Income/IncomeOverview.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { LuPlus } from 'react-icons/lu'
-import CustomBarChart from '../Charts/customBarChart'
+import CustomBarChart from '../Charts/CustomBarChart'
 import { prepareIncomeBarChartData } from '../../utils/helper'
 
 const IncomeOverview = ({transactions, onAddIncome}) => {
